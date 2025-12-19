93c61332160ab50c1498c69dfbb8f8f2202e793a
yehonatan-fisher <yehonatanfisher@gmail.com>
2025-12-19 14:05:42 +0200

    Update Unit03.java
    methods 2 3 and 5

--- Unit03.java
+++ Unit03.java
@@ -3,16 +3,68 @@ public class Unit03{
     public static int shiftRight(int num)    {
         return -1;
     }
+       // (פישר)
+    //method 2
     public static int shiftRight(int num, int k){
         return -1;
+       if (num > 9999 || num < 1000){
+            return -1;
+            }
+        if (k > 4 || k < 0){
+            return -1;
+        }
+        for (int p = 0; k>p ; k--){
+             int last = num % 10;
+             num = num / 10;
+             num += last * 1000;
+         }
+        return num;
     }
+    //(פישר)
+    //method 3
+    public static int shiftRight2(int num, int k){
+         if (num <= 0 || num > 9999)
+            return -1;
+        int digits = 0;
+        for (int temp = num; temp > 0; temp /= 10) {
+            digits++;
+        }
+        if (k < 0 || k > digits)
+            return -1;
+        int multiplier = 1;
+        if (digits == 2)
+            multiplier = 10;
+        if (digits == 3)
+            multiplier = 100;
+        if (digits == 4)
+            multiplier = 1000;
+        for (int p = 0; k>p ; k--){
+             int last = num % 10;
+             num = num / 10;
+             num += last * multiplier;
+         }
+        return num;
    
     public static int shiftLeft(int num)    {
         return -1;
     }
+    // (פישר)
+    // method 5
     public static int shiftLeft(int num, int k){
-        return -1;
-    }
+        if (num > 9999 || num < 1000){
+            return -1;
+            }
+        if (k > 4 || k < 0){
+            return -1;
+        }
+        for (int p = 0; k>p ; k--){
+             int first = num / 1000;
+             num = num % 1000;
+             num *= 10;
+             num += first;
+         }
+        return num;
+        }
     public static String shiftRight(String s)    {
         return "";
     }
